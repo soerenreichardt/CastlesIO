c12b5fbe6828232a714af0c01a452686d447c857
Eike Muecksch <eikem@posteo.de>
2021-04-08 11:22:55 +0200

    use PlayerInfoDTO in LobbyStateDTO instead of just the player names

--- backend/src/main/java/io/castles/core/model/dto/LobbyStateDTO.java
+++ backend/src/main/java/io/castles/core/model/dto/LobbyStateDTO.java
@@ -4,16 +4,17 @@ import io.castles.game.GameLobby;
 import lombok.Value;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Value
 public class LobbyStateDTO {
-    List<String> playerNames;
+    List<PlayerDTO> players;
     String lobbyName;
     LobbySettingsDTO lobbySettings;
 
     public static LobbyStateDTO from(GameLobby gameLobby) {
         return new LobbyStateDTO(
-                gameLobby.getPlayerNames(),
+                gameLobby.getPlayers().stream().map(PlayerDTO::from).collect(Collectors.toList()),
                 gameLobby.getName(),
                 LobbySettingsDTO.from(gameLobby.getLobbySettings())
         );
