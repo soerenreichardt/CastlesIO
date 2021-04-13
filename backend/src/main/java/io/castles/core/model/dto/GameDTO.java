95889ca0e2f15b486e5c041591f3fc43da9629c1
Eike Muecksch <eikem@posteo.de>
2021-04-13 20:46:42 +0200

    Fix GameDTO to use TileDTO in its tiles map

--- backend/src/main/java/io/castles/core/model/dto/GameDTO.java
+++ backend/src/main/java/io/castles/core/model/dto/GameDTO.java
@@ -1,23 +1,27 @@
 package io.castles.core.model.dto;
 
-import io.castles.core.tile.Tile;
 import io.castles.game.Game;
 import lombok.Value;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 @Value
 public class GameDTO {
     GameStateDTO gameState;
-    Map<Integer, Map<Integer, Tile>> tiles;
+    Map<Integer, Map<Integer, TileDTO>> tiles;
     List<PlayerDTO> players;
 
     public static GameDTO from(Game game) {
         return new GameDTO(
                 GameStateDTO.from(game),
-                game.getGameBoardTileMap(),
+                game.getGameBoardTileMap().entrySet().stream().collect(
+                        Collectors.toMap(Entry::getKey, xVal -> xVal.getValue().entrySet().stream().collect(
+                                Collectors.toMap(Entry::getKey, yVal -> TileDTO.from(yVal.getValue()))
+                        ))
+                ),
                 game.getPlayers().stream().map(PlayerDTO::from).collect(Collectors.toList())
         );
     }
