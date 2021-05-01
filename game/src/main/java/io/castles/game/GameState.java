70df2d2fc51caedaec1c20b1143674baac46edb2
Soren <soer3nreichardt@googlemail.com>
2021-05-01 10:18:59 +0200

    Add GAME_END value to GameState enum

--- game/src/main/java/io/castles/game/GameState.java
+++ game/src/main/java/io/castles/game/GameState.java
@@ -5,19 +5,29 @@ public enum GameState {
     DRAW(1),
     PLACE_TILE(2),
     PLACE_FIGURE(3, Flags.SKIPPABLE),
-    NEXT_PLAYER(4);
+    NEXT_PLAYER(4),
+    GAME_END(5, Flags.END_STEP) {
+        @Override
+        public GameState advance() {
+            return GAME_END;
+        }
+    };
 
     static class Flags {
         static final int INITIALIZATION = 1;
         static final int SKIPPABLE = 1 << 1;
+        static final int END_STEP = 1 << 2;
     }
 
     private final int id;
     private int flags;
+    private boolean gameEnd;
 
     GameState(int id, int... flags) {
         this.id = id;
         this.flags = 0;
+        this.gameEnd = false;
+
         for (int flag : flags) {
             this.flags |= flag;
         }
@@ -31,16 +41,29 @@ public enum GameState {
         return (this.flags & Flags.SKIPPABLE) != 0;
     }
 
+    public boolean isEndStep() {
+        return (this.flags & Flags.END_STEP) != 0;
+    }
+
     public GameState advance() {
+        if (gameEnd) {
+            return GAME_END;
+        }
+
         int numGameStates = values().length;
         int nextId = (id + 1) % numGameStates;
         for (int i = 0; i < numGameStates - 1; i++) {
-            if (values()[nextId].isInitializationStep()) {
+            var nextState = values()[nextId];
+            if (nextState.isInitializationStep() || nextState.isEndStep()) {
                 nextId = (nextId + 1) % numGameStates;
             } else {
-                return values()[nextId];
+                return nextState;
             }
         }
         throw new IllegalStateException("Couldn't find the next game state.");
+    }
+
+    public void endGame() {
+        this.gameEnd = true;
     }
 }
